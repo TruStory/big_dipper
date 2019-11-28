38f6b109ba3bf099dbda309402cc3f4c99795a82
jhernandezb <contact@jhernandez.me>
2019-11-28 10:49:24 -0600

    Handle empty attrs

--- imports/ui/components/Activities.jsx
+++ imports/ui/components/Activities.jsx
@@ -63,15 +63,12 @@ export default class Activites extends Component {
 
             // gov
         case "cosmos-sdk/MsgSubmitProposal":
-            let attrs = this.props.events[2].attributes 
-            if (!attrs){
-                console.log('empty attrs', this.props.events[2], this.props.events, this.props.msg)
-                attrs = [{value : -1}]
+            if (this.props.events.length<3 ||  !this.props.events[2] || !this.props.events[2].attributes ){
+                console.log('empty attributes', this.props.events, this.props.msg)
+                let proposalId = `-1`
+                return <p><Account address={msg.value.proposer} /> {(this.props.invalid)?<T>activities.failedTo</T>:''}<MsgType type={msg.type} /> <T>activities.withTitle</T> <Link to={"/proposals/"+proposalId}>{msg.value.content.value.title}</Link><T>common.fullStop</T></p>
             }
-            proposalId = attrs[0].value
-            return <p><Account address={msg.value.proposer} /> {(this.props.invalid)?<T>activities.failedTo</T>:''}<MsgType type={msg.type} /> <T>activities.withTitle</T> <Link to={"/proposals/"+proposalId}>{msg.value.content.value.title}</Link><T>common.fullStop</T></p>
-        case "cosmos-sdk/MsgDeposit":
-            return <p><Account address={msg.value.depositor} /> {(this.props.invalid)?<T>activities.failedTo</T>:''}<MsgType type={msg.type} /> <em className="text-info">{msg.value.amount.map((amount,i) =>new Coin(amount.amount).toString()).join(', ')}</em> <T>activities.to</T> <Link to={"/proposals/"+msg.value.proposal_id}><T>proposals.proposal</T> {msg.value.proposal_id}</Link><T>common.fullStop</T></p>
+            return <p><Account address={msg.value.proposer} /> {(this.props.invalid)?<T>activities.failedTo</T>:''}<MsgType type={msg.type} /> <T>activities.withTitle</T> <Link to={"/proposals/"+this.props.events[2].attributes[0].value}>{msg.value.content.value.title}</Link><T>common.fullStop</T></p>
         case "cosmos-sdk/MsgVote":
             return <p><Account address={msg.value.voter} /> {(this.props.invalid)?<T>activities.failedTo</T>:''}<MsgType type={msg.type} />  <Link to={"/proposals/"+msg.value.proposal_id}><T>proposals.proposal</T> {msg.value.proposal_id}</Link> <T>activities.withA</T> <em className="text-info">{msg.value.option}</em><T>common.fullStop</T></p>
 
